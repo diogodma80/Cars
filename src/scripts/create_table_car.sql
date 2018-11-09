38cda064d44e7acb4b0cc9b92982b125c4b09c5f
Diogo Almeida <diogodma80@gmail.com>
2018-11-09 14:45:58 -0500

    added sql scripts

---
+++ src/scripts/create_table_car.sql
@@ -0,0 +1,11 @@
+create table car
+(id bigint not null auto_increment,
+name varchar(255),
+description varchar(255),
+url_photo varchar(255),
+url_video varchar(255),
+latitude varchar(255),
+longitude varchar(255),
+type varchar(255),
+primary key (id)
+);
